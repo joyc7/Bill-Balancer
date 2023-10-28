4fb4285711c9bc69ec0fd2ec6ecbefbb5324c0df
cindyliang01 <102440420+cindyliang01@users.noreply.github.com>
2023-10-28 19:58:09 -0400

    Revert "Implemented the Friends Page"

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,27 +1,24 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import FriendsPage from './components/FriendsPage';
-
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/friends">Friends</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          {" "}
-          <Route path="/friends" element={<FriendsPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
