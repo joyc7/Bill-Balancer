aa9e1b07592a1c72ec34c0a8cb541597139df8b0
cindyliang01 <cindy.liang023@gmail.com>
2023-10-29 20:13:13 -0400

    Fixed login and home page

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -1,40 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "../styles/Home.css";
-import Navbar from "./Navbar";
-import axios from "axios";
 
 const Home = () => {
-  const [userData, setUserData] = useState([]);
-
-  useEffect(() => {
-    axios("https://my.api.mockaroo.com/users.json?key=0aec1ff0")
-      .then((response) => {
-        setUserData(response.data);
-      })
-      .catch((err) => {
-        console.log(`Sorry, buster. No more requests allowed today!`);
-        console.error(err);
-
-        const backupData = {
-          id: 1,
-          first_name: "Leslie",
-          last_name: "Woodman",
-          total_spending: "$1250",
-        };
-        setUserData(backupData);
-      });
-  }, []);
+  const [totalSpending, setTotalSpending] = useState(1000);
 
   return (
     <div className="home-container">
       <div className="box">
-        <h2 className="total-spending">Total Spending:</h2>
-        <h2>{userData.total_spending}</h2>
+        <h2 className="total-spending">Total Spending</h2>
+        <p className="home-money">${totalSpending}</p>
       </div>
       <div className="box">
         <h2>Groups</h2>
       </div>
-      <Navbar />
     </div>
   );
 };
