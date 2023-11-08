0cc40719bfc4c5aeb4ac5cddf0e4309ce80021f5
Laura Zhao <rz2071@nyu.edu>
2023-11-08 18:11:37 -0500

    merge updates

---
+++ back-end/routes/homeRoute.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  // assemble an object with the data we want to send
+  const body = {
+    id: 1,
+    name: "LA Road Trip",
+    expenses: [
+      {
+        id: 1,
+        name: "Lunch",
+        amount: 358,
+        creator: "Jane",
+        date: "06/16/2023",
+      },
+      {
+        id: 2,
+        name: "Flights to LA",
+        amount: 261,
+        creator: "Tom",
+        date: "01/21/2023",
+      },
+      {
+        id: 3,
+        name: "Hotels",
+        amount: 170,
+        creator: "David",
+        date: "08/02/2023",
+      },
+
+      {
+        id: 4,
+        name: "Dinner",
+        amount: 120,
+        creator: "David",
+        date: "08/02/2023",
+      },
+      {
+        id: 5,
+        name: "Souvenirs",
+        amount: 570,
+        creator: "David",
+        date: "08/02/2023",
+      },
+      {
+        id: 6,
+        name: "Gifts",
+        amount: 200,
+        creator: "David",
+        date: "08/02/2023",
+      },
+    ],
+    description: "Road trip with friends",
+  };
+  // send the response as JSON to the client
+  res.json(body);
+});
+
+module.exports = router;
