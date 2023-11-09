0f126e46d665bf67ebf599a691616b9fa3472dcb
Laura Zhao <rz2071@nyu.edu>
2023-11-08 22:43:32 -0500

    revert master to last pr

--- back-end/app.js
+++ back-end/app.js
@@ -1,9 +1,10 @@
 // import and instantiate express
-const express = require("express") // CommonJS import style!
-const app = express() // instantiate an Express object
-const cors = require('cors');
+const express = require("express"); // CommonJS import style!
+const app = express(); // instantiate an Express object
+const cors = require("cors");
 
-const eventRoute = require('./routes/eventRoute');
+const eventRoute = require("./routes/eventRoute");
+const homeRoute = require("./routes/homeRoute");
 
 app.use(cors());
 
@@ -11,7 +12,8 @@ app.use(cors());
 //     res.send("Hello!")
 // })
 
-app.use('/event', eventRoute);
+app.use("/event", eventRoute);
+app.use("/home", homeRoute);
 
 // export the express app we created to make it available to other modules
-module.exports = app
+module.exports = app;
--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -4,35 +4,91 @@ import Navbar from "./Navbar";
 import axios from "axios";
 
 const Home = () => {
-  const [userData, setUserData] = useState([]);
+  const [backendData, setBackendData] = useState({});
+
+  function calculateTotalSpending(expenses) {
+    return expenses.reduce((total, expense) => total + expense.amount, 0);
+  }
 
   useEffect(() => {
-    axios("https://my.api.mockaroo.com/users.json?key=0aec1ff0")
-      .then((response) => {
-        setUserData(response.data);
-      })
-      .catch((err) => {
-        console.log(`Sorry, buster. No more requests allowed today!`);
+    const fetchHome = async () => {
+      try {
+        const result = await axios.get("http://localhost:3001/home");
+        setBackendData(result.data);
+      } catch (err) {
         console.error(err);
 
         const backupData = {
-          id: 1,
-          first_name: "Leslie",
-          last_name: "Woodman",
-          total_spending: "$1250",
+          id: 2,
+          name: "LA Road Trip",
+          expenses: [
+            {
+              id: 1,
+              name: "Lunch",
+              amount: 358,
+              creator: "Jane",
+              date: "06/16/2023",
+            },
+            {
+              id: 2,
+              name: "Flights to LA",
+              amount: 261,
+              creator: "Tom",
+              date: "01/21/2023",
+            },
+            {
+              id: 3,
+              name: "Hotels",
+              amount: 170,
+              creator: "David",
+              date: "08/02/2023",
+            },
+          ],
+          description: "Road trip with friends",
         };
-        setUserData(backupData);
-      });
+
+        setBackendData(backupData);
+      }
+    };
+
+    fetchHome();
   }, []);
 
   return (
     <div className="home-container">
       <div className="box">
-        <h2 className="total-spending">Total Spending:</h2>
-        <h2>{userData.total_spending}</h2>
-      </div>
-      <div className="box">
-        <h2>Groups</h2>
+        <div>
+          <h2 className="text-center">
+            {backendData &&
+              Array.isArray(backendData.expenses) &&
+              backendData.expenses.length > 0 && (
+                <p>
+                  Total Spending: $
+                  {calculateTotalSpending(backendData.expenses)}
+                </p>
+              )}
+          </h2>
+          <ul className="home-list">
+            {backendData &&
+            backendData.expenses &&
+            backendData.expenses.length > 0 ? (
+              backendData.expenses.map((expense) => (
+                <li key={expense.id} className="home-list2">
+                  <div className="mr-64">
+                    <p className="home-expense-text">{expense.name}</p>
+                  </div>
+                  <div>
+                    <p className="home-expense-amount">
+                      Amount: ${expense.amount}
+                    </p>
+                  </div>
+                </li>
+              ))
+            ) : (
+              <p className="home-expense-amount">No expenses available.</p>
+            )}
+          </ul>
+        </div>
       </div>
       <Navbar />
     </div>
