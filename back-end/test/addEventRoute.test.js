8af14155489ca52ebedd43657f17132fa8ee5b26
elaineZhang67 <144281338+elaineZhang67@users.noreply.github.com>
2023-11-14 12:26:18 -0500

    only write get

--- back-end/test/addEventRoute.test.js
+++ back-end/test/addEventRoute.test.js
@@ -5,30 +5,6 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe('Add Event API', () =>{
-    describe('POST /add-event', () => {
-        it('should return success response when user enter the information', (done) => {
-            let postData = {
-                eventName: "testEventName",
-                date: "testEventData",
-                description: "testEventDescription",
-                member: "testMember"
-            }
-            chai.request(app)
-                .post('/add-event')
-                .send(postData)
-                .end((err, res) => {
-                    expect(err).to.be.null;
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.be.a('object');
-                    expect(res.body).to.have.property('status', 'Success');
-                    expect(res.body).to.have.property('message');
-                    expect(res.body.data).to.deep.equal(postData);
-                    done();
-                });
-        })
-    })
-})
 describe('AddEvent API Endpoint', () => {
     describe('GET /addEventMembers', () => {
         it('should return the user data based on the search', (done) => {
@@ -39,7 +15,7 @@ describe('AddEvent API Endpoint', () => {
                     expect(res).to.have.status(200);
                     expect(res.body).to.be.an('object');
                     expect(res.body).to.deep.equal({
-                        "id": "1",
+                        "id": 1,
                         "name": "Gaby Coupar",
                         "avatar": "https://robohash.org/temporererumomnis.png?size=50x50\u0026set=set1",
                         "phone": "435-715-2899",
