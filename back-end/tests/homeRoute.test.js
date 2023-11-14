01dbc9f6f1c918c6bcee9d5e5601cb9a16de8f02
cindyliang01 <cindy.liang023@gmail.com>
2023-11-13 22:35:44 -0500

    Wrote tests for home and login page

--- back-end/tests/homeRoute.test.js
+++ back-end/tests/homeRoute.test.js
@@ -5,15 +5,17 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe(" GET Endpoint for Login", () => {
-  describe("/GET /", () => {
+describe(" GET Endpoint for Home", () => {
+  describe("/GET /home", () => {
     it("it should return a 200 status code", (done) => {
       chai
         .request(server)
-        .get("/")
+        .get("/home")
         .end((err, res) => {
           expect(res).to.have.status(200);
-
+          expect(res.body).to.be.a("object");
+          expect(res.body).to.have.property("id");
+          expect(res.body).to.have.property("name");
           done();
         });
     });
