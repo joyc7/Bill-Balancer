ca661c2892c3a0e6cb5140fda84be222dea8eb44
HedwigO <rz2071@nyu.edu>
2023-11-20 18:05:52 -0500

    add greetings and dashboard for Home page

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -2,9 +2,28 @@ import React, { useState, useEffect } from "react";
 import "../styles/Home.css";
 import Navbar from "./Navbar";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Home = ({ isDarkMode }) => {
   const [backendData, setBackendData] = useState({});
+  const [userName, setUserName] = useState("");
+
+  const backupNames = {
+    "id": 1,
+    "name": "Bryn",
+    "email": "btaylot0@booking.com",
+    "avatar": "https://robohash.org/utetquibusdam.png?size=50x50\u0026set=set1",
+    "user": [
+        {
+            "id": 5,
+            "name": "Jdavie",
+        },
+        {
+            "id": 2,
+            "name": "Emmie",
+        }
+    ]
+  };
 
   function calculateTotalSpending(expenses) {
     return expenses.reduce((total, expense) => total + expense.amount, 0);
@@ -66,41 +85,64 @@ const Home = ({ isDarkMode }) => {
     fetchHome();
   }, []);
 
+  useEffect(() => {
+    const fetchUserName = async () => {
+      try {
+        const result = await axios.get("http://localhost:3001/user");
+        setUserName(result.data.name); // Assuming the user object has a 'name' property
+      } catch (err) {
+        console.error(err);
+        setUserName(backupNames.name); // Use name from backupData
+      }
+    };
+
+    fetchUserName();
+  }, []);
+
   return (
-    <div className="main">
-      <div className="border">
-        <div>
-          <h2 className="heading2">
-            {backendData &&
-              Array.isArray(backendData.expenses) &&
-              backendData.expenses.length > 0 && (
-                <p>
-                  Total Spending: $
-                  {calculateTotalSpending(backendData.expenses)}
-                </p>
-              )}
-          </h2>
-          <ul className="home-list">
-            {backendData &&
-            backendData.expenses &&
-            backendData.expenses.length > 0 ? (
-              backendData.expenses.map((expense) => (
-                <li key={expense.id} className="small">
-                  <div className="center">
-                    <p className="home-expense-text">{expense.name}</p>
-                    <p className="home-expense-amount">${expense.amount}</p>
-                  </div>
-                </li>
-              ))
+    <div className="home-container">
+      <Navbar />
+      <div className="greeting">
+        <h1>Good Morning, {userName}</h1>
+      </div>
+      <div className="dashboard">
+        <div className="box total-spending">
+          <div className="total-spending-header">
+            <h2 className="heading2">Total Spending:</h2>
+            <p className="heading2 total-amount">${calculateTotalSpending(backendData.expenses || [])}</p>
+          </div>
+          <div className="expenses-list-container">
+            {backendData && backendData.expenses && backendData.expenses.length > 0 ? (
+              <ul className="home-list">
+                {backendData.expenses.map((expense) => (
+                  <li key={expense.id} className="small">
+                    <div className="center">
+                      <p className="home-expense-text">{expense.name}</p>
+                      <p className="home-expense-amount">${expense.amount}</p>
+                    </div>
+                  </li>
+                ))}
+              </ul>
             ) : (
               <p className="home-expense-amount">No expenses available.</p>
             )}
-          </ul>
+          </div>
+        </div>
+        <div className="box events-pending">
+          <h2>Events Pending Payment</h2>
+          <Link to="/events">View All Events</Link>
+          {/* Implement event mapping */}
+        </div>
+        <div className="box friends-pending">
+          <h2>Friends Pending Payment</h2>
+          <Link to="/friends">View All Friends</Link>
+          {/* Implement friend mapping */}
         </div>
       </div>
-      <Navbar />
     </div>
   );
+  
+  
 };
 
 export default Home;
