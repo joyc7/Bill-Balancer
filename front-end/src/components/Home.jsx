50060f01a92660318eb0c41a541617f1f4eef652
HedwigO <rz2071@nyu.edu>
2023-11-20 19:56:59 -0500

    update Expenses Pending Payment dashboard

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -7,7 +7,8 @@ import { Link } from "react-router-dom";
 const Home = ({ isDarkMode }) => {
   const [backendData, setBackendData] = useState({
     expenses: [],
-    friends: []
+    friends: [],
+    events: [] 
   });
   const [userName, setUserName] = useState("");
 
@@ -31,9 +32,26 @@ const Home = ({ isDarkMode }) => {
 
   const backupData = {"id":1,"name":"Bryn","email":"btaylot0@booking.com","phone":"850-479-2094","avatar":"https://robohash.org/utetquibusdam.png?size=50x50\u0026set=set1","friends":[{"id":5,"name":"Jdavie","email":"jzecchinii0@yahoo.co.jp","phone":"967-156-0272","balance":"$57.06"},{"id":2,"name":"Emmie","email":"esworder1@xinhuanet.com","phone":"832-141-0597","balance":"$60.04"}]}; 
 
+  const backupEventsData = {
+    id: 2,
+    name: "LA Road Trip",
+    events: [
+      {"id":1, "name":"Lunch", "amount":358, "creator":"Jane", "date":"06/16/2023"},
+      {"id":2, "name":"Flights to LA", "amount":261, "creator":"Tom", "date":"01/21/2023"},
+      {"id":3, "name":"Hotels", "amount":170, "creator":"David", "date":"08/02/2023"}
+    ],
+    description: "Road trip with friends",
+  }
+
+
   function calculateTotalSpending(expenses) {
     return expenses.reduce((total, expense) => total + expense.amount, 0);
   }
+
+  const eventsWithTotalExpenses = backendData.events ? backendData.events.map(event => ({
+    ...event,
+    totalExpense: calculateTotalSpending(event.expenses || [])
+  })) : [];
 
   /* useEffect for controlling DarkMode of the margin around the page */
   useEffect(() => {
@@ -93,6 +111,36 @@ const Home = ({ isDarkMode }) => {
     fetchHome();
   }, []);
 
+  /* useEffect for fetching events and their expenses */
+  useEffect(() => {
+    const fetchEventsData = async () => {
+      try {
+        const eventsResult = await axios.get("http://localhost:3001/events");
+        if (Array.isArray(eventsResult.data)) {
+          setBackendData(prevData => ({
+            ...prevData,
+            events: eventsResult.data,
+          }));
+        } else {
+          // If it's not an array, log the structure to understand what's being returned
+          console.error('Data fetched is not an array:', eventsResult.data);
+          throw new Error('Data fetched is not an array.');
+        }
+      } catch (error) {
+        console.error('Error fetching events:', error);
+        // Use the array within backupEventsData, not the object itself
+        setBackendData(prevData => ({
+          ...prevData,
+          events: backupEventsData.events, // Ensure this is an array
+        }));
+      }
+    };
+    
+    fetchEventsData();
+  }, []);
+  
+  
+
   /* useEffect for fetching friends and their expenses */
   useEffect(() => {
     const fetchFriendsData = async () => {
@@ -114,8 +162,11 @@ const Home = ({ isDarkMode }) => {
     fetchFriendsData();
   }, []);
 
-  /* since spaces are limited, only display 2 friends, the user can access the rest by clicking "view more" */
+  
+
+  /* since spaces are limited, only display 2 event expenses/friends, the user can access the rest by clicking "view more" */
   const friendsPendingPayment = backendData.friends ? backendData.friends.slice(0, 2) : [];
+  const eventsPending = backupEventsData.events ? backupEventsData.events.slice(0, 2) : [];
 
    /* useEffect for fetching name for greeting */
   useEffect(() => {
@@ -161,8 +212,18 @@ const Home = ({ isDarkMode }) => {
           </div>
         </div>
         <div className="box events-pending">
-          <h2>Events Pending Payment</h2>
-          <Link to="/events" className="view-all">View All</Link>
+      <h2>Expenses Pending Payment</h2>
+      <ul className="home-list">
+  {eventsPending.map(event => (
+    <li key={event.id} className="small">
+      <div className="center">
+        <p className="home-expense-text">{event.name}</p>
+        <p className="home-expense-amount">${event.amount.toFixed(2)}</p>
+      </div>
+    </li>
+  ))}
+</ul>
+      <Link to="/event" className="view-all">View All</Link>
           {/* Implement event mapping */}
         </div>
         <div className="box friends-pending">
