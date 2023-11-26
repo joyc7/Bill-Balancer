37a99f27de3e1d54b58f474f97463bd7cbfc3844
Allison67 <xintongji0@gmail.com>
2023-11-26 17:55:07 -0500

    add validation for add-event-route

--- back-end/routes/addEventRoute.js
+++ back-end/routes/addEventRoute.js
@@ -1,31 +1,46 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 const Event = require("../models/Event.js");
 
-router.post("/", async (req, res) => {
-  try {
-    const newEvent = new Event({
-      name: req.body.eventName,
-      date: req.body.Date,
-      description: req.body.Description,
-      participants: req.body.Members, // Assuming this is an array of User IDs
-      expenses: [], // initialize this as empty
-    });
+router.post(
+  "/",
+  // Validation rules
+  body("eventName").notEmpty().withMessage("Event name is required"),
+  body("Date").notEmpty().withMessage("Date is required"),
+  body("Description").notEmpty().withMessage("Description is required"),
+  body("Members").notEmpty().withMessage("Members are required"),
+  async (req, res) => {
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-    const savedEvent = await newEvent.save();
+    try {
+      const newEvent = new Event({
+        name: req.body.eventName,
+        date: req.body.Date,
+        description: req.body.Description,
+        participants: req.body.Members, // Assuming this is an array of User IDs
+        expenses: [], // initialize this as empty
+      });
 
-    res.status(201).json({
-      status: "Success",
-      message: "Event created successfully!",
-      data: savedEvent,
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: "Error",
-      message: "Error creating event",
-      error: error.message,
-    });
+      const savedEvent = await newEvent.save();
+
+      res.status(201).json({
+        status: "Success",
+        message: "Event created successfully!",
+        data: savedEvent,
+      });
+    } catch (error) {
+      res.status(400).json({
+        status: "Error",
+        message: "Error creating event",
+        error: error.message,
+      });
+    }
   }
-});
+);
 
 module.exports = router;
