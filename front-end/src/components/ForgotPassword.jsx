834b31d3e85480f43fd0277d688dea48846af277
cindyliang01 <cindy.liang023@gmail.com>
2023-11-27 17:16:24 -0500

    Implemented forgot passworfd

--- front-end/src/components/ForgotPassword.jsx
+++ front-end/src/components/ForgotPassword.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Navigate } from "react-router-dom";
 import axios from "axios";
 import "../styles/ForgotPassword.css";
 
 const ForgotPassword = () => {
   const [formData, setFormData] = useState({
     username: "",
+    email: "",
     newPassword: "",
   });
 
   const navigate = useNavigate();
+  const [response, setResponse] = useState({});
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -32,70 +35,80 @@ const ForgotPassword = () => {
           },
         }
       );
+      console.log(`Server response: ${JSON.stringify(response.data, null, 0)}`);
+      setResponse(response.data);
 
-      if (response.status === 200) {
-        console.log("Password reset request successful!");
-        console.log(response.data);
-
-        window.alert("Password reset successful!");
-
-        navigate("/");
+      if (!response.data.success) {
+        setErrorMessage(response.data.message);
       } else {
-        // Handle error responses
-        console.error("Password reset request failed");
+        navigate("/", { state: { showAlert: true } });
       }
     } catch (error) {
-      console.error("Error: " + error.message);
+      setErrorMessage("something went wrong with forgot password!");
     }
   };
 
-  return (
-    <div className="forgot-password-container">
-      <div className="forgot-password-form">
-        <h2 className="forgot-password-title">Forgot Password</h2>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <label htmlFor="username" className="forgot-password-label">
-            Username
-          </label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={formData.username}
-            onChange={handleInputChange}
-            required
-            className="forgot-password-input"
-          />
+  if (!response.success)
+    return (
+      <div className="forgot-password-container">
+        <div className="forgot-password-form">
+          <h2 className="forgot-password-title">Forgot Password</h2>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <label htmlFor="email" className="forgot-password-label">
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              required
+              className="forgot-password-input"
+            />
+            <label htmlFor="username" className="forgot-password-label">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={formData.username}
+              onChange={handleInputChange}
+              required
+              className="forgot-password-input"
+            />
 
-          <label htmlFor="newPassword" className="forgot-password-label">
-            New Password
-          </label>
-          <input
-            type="password"
-            id="newPassword"
-            name="newPassword"
-            value={formData.newPassword}
-            onChange={handleInputChange}
-            required
-            className="forgot-password-input"
-          />
+            <label htmlFor="newPassword" className="forgot-password-label">
+              New Password
+            </label>
+            <input
+              type="password"
+              id="newPassword"
+              name="newPassword"
+              value={formData.newPassword}
+              onChange={handleInputChange}
+              required
+              className="forgot-password-input"
+            />
 
-          <button type="submit" className="forgot-password-button">
-            Reset Password
-          </button>
-        </form>
+            <button type="submit" className="forgot-password-button">
+              Reset Password
+            </button>
+          </form>
 
-        <div className="mt-4">
-          <p>
-            Remember your password?{" "}
-            <a href="/" className="forgot-password-link">
-              Login
-            </a>
-          </p>
+          <div className="mt-4">
+            <p>
+              Remember your password?{" "}
+              <a href="/" className="forgot-password-link">
+                Login
+              </a>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  else return <Navigate to="/" />;
 };
 
 export default ForgotPassword;
