3ea90762225a9b2d94e17199fd4367f53e76e4ed
joyc7 <jc10137@nyu.edu>
2023-11-28 15:10:14 -0500

    Update functionality for add friends

--- front-end/src/components/AddFriendModal.jsx
+++ front-end/src/components/AddFriendModal.jsx
@@ -1,35 +1,72 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import addFriendButton from "../images/plus-button.png"; 
+import { jwtDecode } from 'jwt-decode';
 
 function AddFriendModal({ showModal, onClose }) {
     const [userData, setUserData] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState('');
 
-    const backupUserData = {
-        "avatar": "https://robohash.org/facerevoluptasconsequatur.png?size=50x50\u0026set=set1",
-        "name": "Ulberto Crow"
-    };
-
     async function handleSearchClick() {
+        setUserData(null); 
         setError('');
         setLoading(true);
+        const username = document.querySelector('.input-content').value; 
         try {
-            const response = await fetch("http://localhost:3001/addFriends");
+            const response = await fetch(`http://localhost:3001/searchFriend?username=${username}`);
             if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
+                if (response.status === 404) {
+                    setError('User not found.');
+                } else {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+            } else {
+                const data = await response.json();
+                setUserData(data);
             }
-            const data = await response.json();
-            setUserData(data);
         } catch (error) {
             console.error("Error fetching data:", error);
             setError('Failed to fetch data. Please try again.');
-            setUserData(backupUserData); // Using the backup data on error
         } finally {
             setLoading(false);
         }
     }
 
+    function addFriend(friendUserId) {
+        const token = localStorage.getItem("token");
+        const currentUser = jwtDecode(token);
+    
+        if (!currentUser || !currentUser.id) {
+            console.error("No current user found in local storage.");
+            return;
+        }
+    
+        if (currentUser.id === friendUserId) {
+            setError("You cannot add yourself as a friend.");
+            return;
+        }
+    
+        fetch("http://localhost:3001/addFriends", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ currentUserId: currentUser.id, friendUserId: friendUserId }),
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Could not add friend'); 
+            }
+            return response.text();
+        })
+        .then(message => {
+            setError(message);
+        })
+        .catch(error => {
+            console.error("Error adding friend:", error);
+            setError("Failed to add friend. Please try again.");
+        });
+    }
 
 
     return (
@@ -37,19 +74,25 @@ function AddFriendModal({ showModal, onClose }) {
             <div className="modal-content">
                 <span className="close" onClick={onClose}>&times;</span>
                 <h2>Add a new friend</h2>
-                <input type="text" placeholder="Email or Phone" className='input-content'/>
+                <input type="text" placeholder="Username or Email" className='input-content'/>
                 <button onClick={handleSearchClick}>Search</button>
                 {loading && <div>Loading...</div>}
                 {userData && (
                     <div className="add-friend-item">
-                        <img src={userData.avatar} alt={`${userData.name}'s avatar`} className="friend-avatar"/>
-                        <span className="add-friend-name">{userData.name}</span>
+                        <img src={userData.avatar} alt={`avatar`} className="friend-avatar"/>
+                        <span className="add-friend-name">{userData.username}</span>
                         <img 
                             src={addFriendButton}
                             alt="Add friend"
                             className="add-friend-button"
                             style={{ width: "50px", height: "50px" }}
+                            onClick={() => addFriend(userData._id)}
                         />
+                    </div>
+                )}
+                {error && (
+                    <div className="error-message">
+                        {error}
                     </div>
                 )}
             </div>
@@ -58,4 +101,3 @@ function AddFriendModal({ showModal, onClose }) {
 }
 
 export default AddFriendModal;
-
