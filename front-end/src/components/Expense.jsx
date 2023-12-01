041dcbcc8b066674c4574470f0e3b1f16acc5792
joyc7 <jc10137@nyu.edu>
2023-12-01 05:31:25 -0500

    Debug

--- front-end/src/components/Expense.jsx
+++ front-end/src/components/Expense.jsx
@@ -4,23 +4,21 @@ import React, { useState, useEffect } from 'react';
 import '../styles/Expense.css';
 import axios from "axios";
 import Navbar from "./Navbar";
-import { Link, useNavigate, useParams } from "react-router-dom"; {/* useNavigate is used to direct the user to the previous page */}
+import { Link, useNavigate, useParams } from "react-router-dom"; 
+
 
 function Expense({ isDarkMode }) {
     const [expensesData, setExpensesData] = useState([]);
     const navigate = useNavigate();
     const { expenseId } = useParams();
-    console.log("expenseId:", expenseId)
 
     const fetchData = async () => {
         try {
             const response = await axios.get(`http://localhost:3001/expense/ExpenseDetail/${expenseId}`);
-            console.log(response)
+            console.log(response.data); 
             setExpensesData(response.data);
         }catch(error){
             console.error("There was an error fetching the data:", error);
-            console.log(backupData_expenses)
-            setExpensesData(backupData_expenses)
         }
     };
 
@@ -42,16 +40,6 @@ function Expense({ isDarkMode }) {
         fetchData();
     }, []);
 
-    const backupData_expenses = {
-        "expense": [
-            {"id":1,"name":"Jolynn","expense":"$246.92"},
-            {"id":2,"name":"Merv","expense":"$210.86"},
-            {"id":3,"name":"Cora","expense":"$18.65"},
-            {"id":4,"name":"Dwain","expense":"$200.25"},
-            {"id":5,"name":"Husain","expense":"$478.25"}
-        ]
-    };
-
     {/* navigates to the previous page */}
     const handleTitleClick = () => {
         navigate(-1); 
@@ -69,7 +57,7 @@ function Expense({ isDarkMode }) {
                 {/* Default: assume all expenses are positive */}
                 <div className="expense-list">
                     {/* Assume a headcount limit to split the bill */}
-                    {expensesData.slice(0, 7).map(item => (
+                    {Array.isArray(expensesData) && expensesData.slice(0, 7).map(item => (
                     <div key={item.id} className="expense-item">
                         <span>{item.name}</span> {/* This is the first child */}
                         <span className={parseFloat(item.expense.replace('$', '')) > 0 ? 'positive' : 'negative'}>{item.expense}</span> {/* This is the second child */}
