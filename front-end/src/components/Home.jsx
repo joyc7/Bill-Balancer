e00f84227a41fa042bf1b4390692c19a3b0b7c05
Allison67 <xintongji0@gmail.com>
2023-12-01 14:24:21 -0500

    Update Home.jsx

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -239,9 +239,6 @@ const Home = ({ isDarkMode }) => {
               </li>
             ))}
           </ul>
-          {/* <Link to="/event" className="view-all">
-            View All
-          </Link> */}
         </div>
         <div className="box friends-pending">
           <h2 className="heading2">Friends Summary</h2>
