3a77a4917ab506809a2726634b8a5475f318a735
joyc7 <jc10137@nyu.edu>
2023-12-02 17:24:24 -0500

    Update searchFriendRoute.js
    Make the search not case sensitive

--- back-end/routes/searchFriendRoute.js
+++ back-end/routes/searchFriendRoute.js
@@ -5,9 +5,10 @@ const { User } = require("../models/User.js");
 router.get("/", async (req, res) => {
   const { username } = req.query;
   try {
-    const userData = await User.findOne({ username: username }).select(
-      "username avatar _id"
-    );
+    const userData = await User.findOne({
+      username: new RegExp("^" + username + "$", "i"),
+    }).select("username avatar _id");
+
     if (!userData) {
       return res.status(404).send("User not found");
     }
