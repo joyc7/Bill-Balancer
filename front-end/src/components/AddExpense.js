9f1e0807de410730d3bb490abca758f38ec86d04
joyc7 <jc10137@nyu.edu>
2023-12-05 01:00:47 -0500

    Update AddExpense.js

--- front-end/src/components/AddExpense.js
+++ front-end/src/components/AddExpense.js
@@ -101,6 +101,23 @@ const AddExpense = (props) => {
 
   const handleAmountsChange = (amounts) => {
     setIndividualAmounts(amounts);
+
+    // Check all amounts are valid now
+    let allAmountsValid = true;
+    for (const amount of Object.values(amounts)) {
+      if (typeof amount !== "number" || isNaN(amount) || amount <= 0) {
+        allAmountsValid = false;
+        break;
+      }
+    }
+
+    // Clear the error message if all amounts are valid
+    if (allAmountsValid) {
+      setValidationMessages((prevMessages) => ({
+        ...prevMessages,
+        individualAmounts: "",
+      }));
+    }
   };
 
   const handleAddExpense = async () => {
@@ -148,11 +165,14 @@ const AddExpense = (props) => {
 
     const amountNumber = parseFloat(formData.amount);
 
+    let allAmountsValid = true;
+
     const peopleSplit = Array.isArray(selectedPeople)
       ? selectedPeople.map((person) => {
           const amount = individualAmounts[person._id];
-          if (typeof amount !== "number" || isNaN(amount)) {
-            invalidAmounts = true;
+          if (typeof amount !== "number" || isNaN(amount) || amount <= 0) {
+            invalidAmounts = true; // Found an invalid amount
+            allAmountsValid = false; // Set the flag to false as we found an invalid amount
           }
           return {
             user: person,
@@ -164,8 +184,12 @@ const AddExpense = (props) => {
     if (invalidAmounts) {
       newValidationMessages.individualAmounts =
         "Invalid amount(s) in split details";
-      isValid = false;
+    } else {
+      // If all amounts are valid, clear the error message
+      newValidationMessages.individualAmounts = "";
     }
+
+    isValid = !invalidAmounts;
 
     setValidationMessages(newValidationMessages);
 
