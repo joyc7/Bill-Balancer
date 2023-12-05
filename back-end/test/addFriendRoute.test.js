3840504283cc5059eb240bd9f07ce6767e31ed65
joyc7 <jc10137@nyu.edu>
2023-12-05 17:59:25 -0500

    Update addFriendRoute.test.js

--- back-end/test/addFriendRoute.test.js
+++ back-end/test/addFriendRoute.test.js
@@ -1,24 +1,37 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const expect = chai.expect;
-const app = require('../app'); 
-
+const { expect } = require("chai");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../app");
 chai.use(chaiHttp);
 
-describe('Add Friend Route', function() {
-  describe('GET /addFriends', function() {
-    it('should return data of a user as the result of a search', function(done) {
-      chai.request(app)
-        .get('/addFriends')
-        .end(function(err, res) {
+describe("Add Friend API", () => {
+  describe("POST /addFriends - One or Both Users Not Found", () => {
+    it("should return an error if one or both users are not found", (done) => {
+      const currentUserId = "507f1f77bcf86cd799439011"; // nonexistentUserId
+      const friendUserId = "656d6bb42bda8464c472ed7f"; // valid ID
+      chai
+        .request(app)
+        .post("/addFriends")
+        .send({ currentUserId, friendUserId })
+        .end((err, res) => {
+          expect(res).to.have.status(404);
+          expect(res.text).to.equal("One or both users not found");
+          done();
+        });
+    });
+  });
+
+  describe("POST /addFriends - Users Already Friends", () => {
+    it("should return a message if users are already friends", (done) => {
+      const currentUserId = "656d6bb42bda8464c472ed7f"; // valid IDs already friends
+      const friendUserId = "656d6afdb41784541809ef05"; // valid IDs already friends
+      chai
+        .request(app)
+        .post("/addFriends")
+        .send({ currentUserId, friendUserId })
+        .end((err, res) => {
           expect(res).to.have.status(200);
-          expect(res.body).to.be.an('object');
-          expect(res.body).to.deep.equal({
-            "name": "Sallyanne",
-            "email": "sgoseling0@eepurl.com",
-            "phone": "764-995-4333",
-            "avatar": "https://robohash.org/eligendiquiased.png?size=50x50&set=set1"
-          });
+          expect(res.text).to.equal("Already friends");
           done();
         });
     });
