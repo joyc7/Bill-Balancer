adcb18318a7db81eb2a5eabd1bb43ff583f4396e
Allison67 <xintongji0@gmail.com>
2023-12-05 23:56:33 -0500

    Update Event.js

--- back-end/Models/Event.js
+++ back-end/Models/Event.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const User = require("./User");
-const Expense = require("./Expense");
+const { User } = require("./User");
+const { Expense } = require("./Expense");
 
 const eventSchema = new Schema({
   name: {
