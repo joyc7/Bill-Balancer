f5244f9d5dd85729bea01abe9cd55d180f5a550a
joyc7 <jc10137@nyu.edu>
2023-12-05 21:06:11 -0500

    Update addEventMemberRoute.test.js

--- back-end/test/addEventMemberRoute.test.js
+++ back-end/test/addEventMemberRoute.test.js
@@ -1,23 +1,48 @@
-const chai = require('chai');
+const chai = require("chai");
 const chaiHttp = require("chai-http");
-const app = require('../app')
-const expect = chai.expect;
+const app = require("../app");
+const { expect } = chai;
 
 chai.use(chaiHttp);
 
-describe('AddEventMember API Endpoint', () => {
-    describe('GET /addEventMember', () => {
-        it('should return the user data based on the search', (done) => {
-            chai.request(app)
-                .get('/addEventMember')
-                .end((err, res) => {
-                    expect(err).to.be.null;
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.be.an('array');
-                    expect(res.body[0]).to.have.all.keys('id', 'name', 'avatar','phone','email');
-                    done();
-                });
-        });
-    });
-});
+describe("GET /addEventMember/friendsList/:userId - Get User's Friends List", () => {
+  it("should return a user's friends list for a valid user ID", (done) => {
+    const validUserId = "656d6bb42bda8464c472ed7f";
 
+    chai
+      .request(app)
+      .get(`/addEventMember/friendsList/${validUserId}`)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an("object");
+        expect(res.body).to.have.property("_id", validUserId);
+        expect(res.body).to.have.property("avatar").to.be.an("string");
+        expect(res.body).to.have.property("email").to.be.an("string");
+        expect(res.body).to.have.property("username").to.be.an("string");
+        expect(res.body).to.have.property("events").to.be.an("array");
+        expect(res.body).to.have.property("friends").to.be.an("array");
+        const friendsList = res.body.friends;
+        for (const friend of friendsList) {
+          expect(friend).to.have.property("avatar").to.be.a("string");
+          expect(friend).to.have.property("email").to.be.a("string");
+          expect(friend).to.have.property("username").to.be.a("string");
+          expect(friend).to.have.property("events").to.be.an("array");
+          expect(friend).to.have.property("friends").to.be.an("array");
+        }
+        done();
+      });
+  });
+
+  it("should return a 404 error for an invalid user ID", (done) => {
+    const invalidUserId = "507f1f77bcf86cd799439011";
+
+    chai
+      .request(app)
+      .get(`/addEventMember/friendsList/${invalidUserId}`)
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        expect(res.text).to.equal("User not found");
+        done();
+      });
+  });
+});
